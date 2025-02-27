82378722b7740ab79908d7d56c7b14cf8008795c
Alexandre liberato <alexandreliberatto@gmail.com>
2025-02-27 18:02:14 -0300

    feat(login): Criando um filtro de segurança.
    Signed-off-by: Alexandre liberato <alexandreliberatto@gmail.com>

---
+++ src/main/java/com/academia/academia/config/Filtern.java
@@ -0,0 +1,27 @@
+package com.academia.academia.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class Filtern extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        var Token = buscarToken(request);
+    }
+
+    private String buscarToken(HttpServletRequest request){
+        var authorization = request.getHeader("Authorization");
+        if (authorization == null){
+            throw new RuntimeException("Token não encontrado!");
+        }
+        return authorization.replace("Bearer ", "");
+    }
+}
