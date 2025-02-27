936b73dc0283951092abfa6966ecb3657c4db15d
Alexandre liberato <alexandreliberatto@gmail.com>
2025-02-27 19:14:23 -0300

    feat(login): Recuperando usuário no token JWT.
    Signed-off-by: Alexandre liberato <alexandreliberatto@gmail.com>

--- src/main/java/com/academia/academia/config/Filtern.java
+++ src/main/java/com/academia/academia/config/Filtern.java
@@ -1,20 +1,29 @@
 package com.academia.academia.config;
 
+import com.academia.academia.service.TokenService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class Filtern extends OncePerRequestFilter {
+
+    private final TokenService tokenService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        var Token = buscarToken(request);
+        String token = buscarToken(request);
+        String usuarioLogin = tokenService.buscarUsuarioToken(token);
+
+        filterChain.doFilter(request, response);
     }
 
     private String buscarToken(HttpServletRequest request){
--- src/main/java/com/academia/academia/service/TokenService.java
+++ src/main/java/com/academia/academia/service/TokenService.java
@@ -4,6 +4,7 @@ import com.academia.academia.model.Usuario;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -27,5 +28,20 @@ public class TokenService {
             throw new RuntimeException("Erro ao criar token", e);
         }
 
+    }
+
+    public String buscarUsuarioToken(String token){
+        try {
+            Algorithm algoritmo = Algorithm.HMAC256("1234");
+
+            return JWT.require(algoritmo)
+                    .withIssuer("Alexandre Liberato")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+
+        } catch (JWTVerificationException ex){
+            throw new RuntimeException("Token incorreto!");
+        }
     }
 }
